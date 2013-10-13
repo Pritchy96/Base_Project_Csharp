9fed0e3894b4047989465701a769c8c28ae5a5e4
Tom Pritchard <tompritchard1996@gmail.com>
2013-10-13 17:18:33 +0100

    Basic Project
    To Be Added
    Sound System
    Better Input System

---
+++ Base Project (Always Copy!)/States/BasicState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base_Project__Always_Copy__
+{
+    abstract class BasicState
+    {
+
+        public virtual void Update()
+        {
+        }
+
+        public virtual void Redraw(PaintEventArgs e)
+        {
+        }
+
+        public virtual void MouseMoved(MouseEventArgs e)
+        {
+        }
+
+        public virtual void MouseClicked(MouseEventArgs e)
+        {
+        }
+
+    }
+}
---
+++ Base Project (Always Copy!)/States/MenuState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base_Project__Always_Copy__
+{
+    class MenuState : BasicState
+    {
+        public override void Update()
+        {
+        }
+
+        public override void Redraw(PaintEventArgs e)
+        {
+        }
+
+        public override void MouseMoved(MouseEventArgs e)
+        {
+        }
+
+        public override void MouseClicked(MouseEventArgs e)
+        {
+        }
+    }
+}
